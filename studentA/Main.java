e70f13244440dca8fd54fcfd28cd3d50af070df1
Yu, Subin <soobin45696@gmail.com>
2022-10-24 15:36:55 +0900

    Main.java 수정3

--- studentA/Main.java
+++ studentA/Main.java
@@ -3,5 +3,7 @@ public class Main {
     public static void main(String[] args) { 
         String s = "안녕하세요"; 
         System.out.println(s); 
+        Hello hello = new Hello(); 
+        hello.say(); 
     } 
 }
