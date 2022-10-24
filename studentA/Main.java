e0bdadbe1a6de9f2987114fb72c554e1dccfa08a
Yu, Subin <soobin45696@gmail.com>
2022-10-24 15:36:13 +0900

    Main.java 수정2

--- studentA/Main.java
+++ studentA/Main.java
@@ -1,5 +1,7 @@
 public class Main { 
+    
     public static void main(String[] args) { 
-        System.out.println("hello world");
+        String s = "안녕하세요"; 
+        System.out.println(s); 
     } 
 }
